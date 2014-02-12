b35556bfc0ccb3f5f3c15de3c76a2c21c794bd86
Sahil Patel <spatel7@berkeley.edu>
2014-02-12 02:31:05 -0800

    Groups now have their own pages.

--- mainstream_project/mainstream/urls.py
+++ mainstream_project/mainstream/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from mainstream import views
 
 urlpatterns = patterns('',
-			url(r'^$', views.index, name='index'))
+			url(r'^$', views.index, name='index'),
+			url(r'^add_group/$', views.add_group, name='add_group'),
+			url(r'^group/(?P<group_id_url>\w+)/$', views.group, name='group'))
--- mainstream_project/mainstream/views.py
+++ mainstream_project/mainstream/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from mainstream.models import Group, Stream, Post
+from mainstream.forms import GroupForm
+from django.utils import timezone
 
 def index(request):
 	context = RequestContext(request)
@@ -10,3 +12,31 @@ def index(request):
 	context_dict['streams'] = Stream.objects.all()
 	context_dict['posts'] = Post.objects.all()
 	return render_to_response("mainstream/index.html", context_dict, context)
+
+def group(request, group_id_url):
+	context = RequestContext(request)
+	group_id = group_id_url
+	context_dict = {'group_id': group_id}
+	try:
+		group = Group.objects.get(id=group_id)
+		streams = Stream.objects.filter(group=group)
+		context_dict['streams'] = streams
+		context_dict['group'] = group
+	except Group.DoesNotExist:
+		pass
+	return render_to_response("mainstream/group.html", context_dict, context)
+
+def add_group(request):
+	context = RequestContext(request)
+	if request.method == 'POST':
+		group_form = GroupForm(request.POST)
+		if group_form.is_valid():
+			group = group_form.save(commit=False)
+			group.date = timezone.now()
+			group.save()
+			return HttpResponseRedirect("/mainstream/")
+		else:
+			print group_form.errors
+	else:
+		group_form = GroupForm
+	return render_to_response("mainstream/add_group.html", {'group_form': group_form}, context)
